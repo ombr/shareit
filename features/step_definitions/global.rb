3df04198cbbedeb4103daba2f63e6ad6d3961273
Luc Boissaye <luc@boissaye.fr>
2013-09-10 22:10:45 +0200

    Oups, move password to env :-D

--- features/step_definitions/global.rb
+++ features/step_definitions/global.rb
@@ -5,8 +5,8 @@ When(/^I import my box\.com account$/) do
   visit root_path
   click_link 'Import'
   click_link 'Box'
-  fill_in 'login', with: 'ombr@ombr.net'
-  fill_in 'password', with: ''
+  fill_in 'login', with: ENV['TEST_BOX_LOGIN']
+  fill_in 'password', with: ENV['TEST_BOX_PASSWORD']
   find('.login_submit').click
   find('#consent_accept_button').click
   visit current_url.gsub(ENV['BOX_AUTHORIZE_URL'], box_imports_path)# We remove https for easy testing :-D
